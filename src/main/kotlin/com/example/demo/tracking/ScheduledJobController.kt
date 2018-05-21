0be794de4d4c73bfe053dd66e7ae9c377afbabc4
Kevin <k.grueneberg1994@gmail.com>
2018-05-21 20:45:20 +0200

    Filter functions in tracker, adjustments to controller

--- src/main/kotlin/com/example/demo/tracking/ScheduledJobController.kt
+++ src/main/kotlin/com/example/demo/tracking/ScheduledJobController.kt
@@ -1,6 +1,7 @@
 package com.example.demo.tracking
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -15,12 +16,12 @@ class ScheduledJobController @Autowired constructor(
     @RequestMapping(method = [RequestMethod.GET])
     fun getScheduledJobs(): ResponseEntity<Any> {
         val scheduledJobs = scheduledJobTracker.trackedJobs
-        return ResponseEntity.ok(scheduledJobTracker.trackedJobs)
+        return ResponseEntity.ok(scheduledJobs)
     }
 
     @RequestMapping(value = ["{className}"], method = [RequestMethod.GET])
     fun getScheduledJobsPerClass(@PathVariable(name = "className") className: String): ResponseEntity<Any> {
-        val scheduledJobs = scheduledJobTracker.trackedJobs.filter { it.className == className }
+        val scheduledJobs = scheduledJobTracker.findJobsByClass(className)
         return ResponseEntity.ok(scheduledJobs)
     }
 
@@ -29,9 +30,24 @@ class ScheduledJobController @Autowired constructor(
         @PathVariable(name = "className") className: String,
         @PathVariable(name = "methodName") methodName: String
     ): ResponseEntity<Any> {
-        val scheduledJobs =
-            scheduledJobTracker.trackedJobs.filter { it.className == className && it.methodName == methodName }
-        return ResponseEntity.ok(scheduledJobs.first())
+        val scheduledJob = scheduledJobTracker.findJobByClassAndMethod(className, methodName)
+        if (scheduledJob == null)
+            return ResponseEntity(HttpStatus.NOT_FOUND)
+
+        return ResponseEntity.ok(scheduledJob)
+    }
+
+    @RequestMapping(value = ["{className}/{methodName}/{uuid}"], method = [RequestMethod.GET])
+    fun getScheduledJobRun(
+        @PathVariable(name = "className") className: String,
+        @PathVariable(name = "methodName") methodName: String,
+        @PathVariable(name = "uuid") uuid: String
+    ): ResponseEntity<Any> {
+        val scheduledJobRun = scheduledJobTracker.findRunByUuid(className, methodName, uuid)
+        if (scheduledJobRun == null)
+            return ResponseEntity(HttpStatus.NOT_FOUND)
+
+        return ResponseEntity.ok(scheduledJobRun)
     }
 
 }
--- src/main/kotlin/com/example/demo/tracking/ScheduledJobTracker.kt
+++ src/main/kotlin/com/example/demo/tracking/ScheduledJobTracker.kt
@@ -1,21 +1,56 @@
 package com.example.demo.tracking
 
 import org.aspectj.lang.reflect.MethodSignature
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.scheduling.config.ScheduledTaskHolder
+import org.springframework.scheduling.support.ScheduledMethodRunnable
 import org.springframework.stereotype.Component
 import java.time.Instant
 import java.util.*
 import kotlin.collections.HashSet
 
 @Component
-class ScheduledJobTracker {
+class ScheduledJobTracker @Autowired constructor(
+    private val scheduledTaskHolder: ScheduledTaskHolder
+) {
 
     var trackedJobs: MutableSet<TrackedScheduledJob> = HashSet()
+
+    init {
+        initJobs()
+    }
+
+    private fun initJobs() {
+        val scheduledTasks = scheduledTaskHolder.scheduledTasks
+        scheduledTasks.forEach {
+            val runnable = it.task.runnable as ScheduledMethodRunnable
+            val annotation = runnable.method.getAnnotation(
+                Scheduled::class.java
+            )
+
+            trackedJobs.add(
+                TrackedScheduledJob(
+                    className = runnable.method.declaringClass.name,
+                    methodName = runnable.method.name,
+                    settings = Settings(
+                        cron = nullIfEmptyString(annotation.cron),
+                        fixedRate = nullIfNegativeNumber(annotation.fixedRate),
+                        fixedRateString = nullIfEmptyString(annotation.fixedRateString),
+                        initialDelay = nullIfNegativeNumber(annotation.initialDelay),
+                        initialDelayString = nullIfEmptyString(annotation.initialDelayString),
+                        fixedDelay = nullIfNegativeNumber(annotation.fixedDelay),
+                        fixedDelayString = nullIfEmptyString(annotation.fixedDelayString)
+                    )
+                )
+            )
+        }
+    }
 
     fun jobStart(signature: MethodSignature): UUID {
         val uuid = UUID.randomUUID()
 
-        val scheduledJob = getOrAddJob(signature)
+        val scheduledJob = get(signature)
         scheduledJob.addRun(
             ScheduledJobRun(
                 uuid = uuid,
@@ -26,38 +61,18 @@ class ScheduledJobTracker {
         return uuid
     }
 
-    private fun getOrAddJob(signature: MethodSignature): TrackedScheduledJob {
+    private fun get(signature: MethodSignature): TrackedScheduledJob {
         val className = signature.declaringTypeName
         val methodName = signature.method.name
 
-        var trackedJob = trackedJobs.find { it.className == className && it.methodName == methodName }
-
-        if (trackedJob == null) {
-            trackedJob = addJob(signature)
-            trackedJobs.add(trackedJob)
-        }
-
-        return trackedJob
+        return trackedJobs.find { it.className == className && it.methodName == methodName }!!
     }
 
-    private fun addJob(signature: MethodSignature): TrackedScheduledJob {
-        val annotation = signature.method.getAnnotation(
-            Scheduled::class.java
-        )
-
-        return TrackedScheduledJob(
-            className = signature.declaringTypeName,
-            methodName = signature.method.name,
-            settings = Settings(
-                cron = nullIfEmptyString(annotation.cron),
-                fixedRate = nullIfNegativeNumber(annotation.fixedRate),
-                fixedRateString = nullIfEmptyString(annotation.fixedRateString),
-                initialDelay = nullIfNegativeNumber(annotation.initialDelay),
-                initialDelayString = nullIfEmptyString(annotation.initialDelayString),
-                fixedDelay = nullIfNegativeNumber(annotation.fixedDelay),
-                fixedDelayString = nullIfEmptyString(annotation.fixedDelayString)
-            )
-        )
+    fun jobEnd(uuid: UUID, signature: MethodSignature, exception: Throwable?) {
+        val className = signature.declaringTypeName
+        val methodName = signature.method.name
+        val trackedJob = trackedJobs.find { it.className == className && it.methodName == methodName }!!
+        trackedJob.endRun(uuid, exception)
     }
 
     private fun nullIfEmptyString(str: String): String? {
@@ -68,12 +83,20 @@ class ScheduledJobTracker {
         return if (number < 0) null else number
     }
 
-    fun jobEnd(uuid: UUID, signature: MethodSignature, exception: Throwable?) {
-        val className = signature.declaringTypeName
-        val methodName = signature.method.name
-        val trackedJob = trackedJobs.find { it.className == className && it.methodName == methodName }!!
-        trackedJob.endRun(uuid, exception)
+    fun findJobsByClass(className: String): List<TrackedScheduledJob> {
+        return trackedJobs.filter { it.className == className }
     }
 
+    fun findJobByClassAndMethod(className: String, methodName: String): TrackedScheduledJob? {
+        return trackedJobs.firstOrNull { it.className == className && it.methodName == methodName }
+    }
+
+    fun findRunByUuid(className: String, methodName: String, uuid: String): ScheduledJobRun? {
+        val scheduledJob = findJobByClassAndMethod(className, methodName)
+        if (scheduledJob == null)
+            return null
+
+        return scheduledJob.runs.firstOrNull { it.uuid.toString() == uuid }
+    }
 }
 
