a5536abaadd48866f0434898f279844364298c0b
Kevin <k.grueneberg1994@gmail.com>
2018-05-22 22:49:41 +0200

    Allow cross origin

--- src/main/kotlin/com/example/demo/tracking/ScheduledJobController.kt
+++ src/main/kotlin/com/example/demo/tracking/ScheduledJobController.kt
@@ -4,12 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 
 @Controller
 @RequestMapping("\${tracking.scheduledJobs.path:/scheduled-jobs}")
+@CrossOrigin(origins = ["*"])
 class ScheduledJobController @Autowired constructor(
     private val scheduledJobTracker: ScheduledJobTracker
 ) {
