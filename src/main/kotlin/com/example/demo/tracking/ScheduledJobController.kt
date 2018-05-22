90577a6c9766ca76d551c6dfb80341bc98831ef5
Kevin <k.grueneberg1994@gmail.com>
2018-05-22 21:44:22 +0200

    Make URL for scheduled job controller configurable

--- src/main/kotlin/com/example/demo/tracking/ScheduledJobController.kt
+++ src/main/kotlin/com/example/demo/tracking/ScheduledJobController.kt
@@ -3,12 +3,13 @@ package com.example.demo.tracking
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
-import org.springframework.web.bind.annotation.RestController
 
-@RestController
+@Controller
+@RequestMapping("\${tracking.scheduledJobs.path:/scheduled-jobs}")
 class ScheduledJobController @Autowired constructor(
     private val scheduledJobTracker: ScheduledJobTracker
 ) {
